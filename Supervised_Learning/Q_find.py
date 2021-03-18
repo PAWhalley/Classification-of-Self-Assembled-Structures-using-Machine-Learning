13d3317e269e212736626f788a1add85c01d7076
conor012 <73125833+conor012@users.noreply.github.com>
2021-03-18 21:52:21 +0000

    Add files via upload

--- Supervised_Learning/Q_find.py
+++ Supervised_Learning/Q_find.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
+from scipy.spatial.distance import cdist
 
 # Function to calculate Q
 R=0.1
 
-# Function to calculate if points x and y are withing R of each other
+# Function to calculate if points x and y are within R of each other
 def charfun(x,y, R):
     if min(abs(x-y), 1 - abs(x-y)) <= R:
         return 1
@@ -12,9 +13,24 @@ def charfun(x,y, R):
 
 # Function that takes in the some number of point clouds in the form
 #  of trainset, R and the number of points in each cloud
-# and returns an array with the all the valus of Q in a list.
+# and returns an array with the all the values of Q in a list.
 def Q_finder(trainset, R, npoints):
     labels = []
     for x in trainset.view(-1,npoints).detach().numpy():
         labels.append(1/(len(x)**2) * sum(charfun(k, j, R) for k in x for j in x))
     return labels
+
+
+def Q_findmat(trainset, R, npoints):
+    trainset = trainset.view(-1,npoints, 1)
+    labels = []
+    counter = 0
+    n_squared = npoints * npoints
+    for x in trainset:
+        counter += 1
+        dist_mat = cdist(x,x, lambda x,y:min(abs(x-y), 1 - abs(x-y)))
+        dist_mat[dist_mat <= R,], dist_mat[dist_mat> R] = 0, 1
+        labels.append((1/n_squared) * (n_squared - (dist_mat.sum(axis = 0)).sum(axis = 0)))
+    return labels
+    
+
